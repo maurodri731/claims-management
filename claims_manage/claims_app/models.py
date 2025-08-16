39b50a819fd9b4a8cb7f32a1b5c64a2b8ec312c7
Mauricio <mxr210005@utdallas.edu>
2025-08-16 10:14:56 -0500

    initial commit

---
+++ claims_manage/claims_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class ClaimList(models.Model):
+    patient_name = models.CharField(max_length=200)
+    billed_amount = models.DecimalField(max_digits=15, decimal_places=2)
+    paid_amount = models.DecimalField(max_digits=15, decimal_places=2)
+    status = models.CharField(max_length=20)
+    insurer_name = models.CharField(max_length=50)
+    discharge_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.patient_name} - {self.billed_amount} - {self.paid_amount} - {self.status} - {self.insurer_name} on {self.discharge_date}"
+    
+class ClaimDetails(models.Model):
+    claim_id = models.ForeignKey(ClaimList, on_delete=models.CASCADE, related_name="claims")
+    denial_reason = models.CharField(max_length=200, null=True)
+    cpt_codes= models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.claim_id} - {self.denial_reason} - {self.cpt_codes}"
